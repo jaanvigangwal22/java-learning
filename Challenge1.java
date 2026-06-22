182f58389d8fcbe6424f2d050b11d89ea0fb22fb
jaanvigangwal22 <jaanvi.gangwal04@gmail.com>
2026-06-22 23:18:21 +0530

    challenge 1 completed

---
+++ Challenge1.java
@@ -0,0 +1,7 @@
+public class Challenge1 {
+    public static void main (String[] args){
+     // Print name and age w3schools
+        System.out.println("My name is Jaanvi Gangwal ");
+        System.out.println("My age is 20");
+    }
+}
