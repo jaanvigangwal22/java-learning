d6adf4a81b3df011b6dc14a3d5933f661526aaad
jaanvigangwal22 <jaanvi.gangwal04@gmail.com>
2026-07-15 22:04:03 +0530

    Day 3 completed

--- Day3.java
+++ Day3.java
@@ -5,27 +5,43 @@ public class Day3 {
 
         Scanner in = new Scanner(System.in);
 
-        //QUESTION 1 - CHECK IF A NUMBER IS EVEN OR ODD 
-        System.out.print("Enter your number : ");
-        int n = in.nextInt();
-        String result = (n % 2== 0) ? "Even":"odd"; // shorthand if else used 
-        System.out.println(result);
+        // //QUESTION 1 - CHECK IF A NUMBER IS EVEN OR ODD 
+        // System.out.print("Enter your number : ");
+        // int n = in.nextInt();
+        // String result = (n % 2== 0) ? "Even":"odd"; // shorthand if else used 
+        // System.out.println(result);
 
-        //QUESTION 2 = SIMPLE INTEREST 
-        System.out.print(" Enter  amount , rate , time : ");
-        double p = in.nextDouble(); //principal amount
-        double r = in.nextDouble();// rate of interest 
-        double t = in.nextDouble(); // time 
+        // //QUESTION 2 = SIMPLE INTEREST 
+        // System.out.print(" Enter  amount , rate , time : ");
+        // double p = in.nextDouble(); //principal amount
+        // double r = in.nextDouble();// rate of interest 
+        // double t = in.nextDouble(); // time 
 
-        double sI =( ( p*r*t) /100 );
-        System.out.println("Simple Interest is : "  + sI );
+        // double sI =( ( p*r*t) /100 );
+        // System.out.println("Simple Interest is : "  + sI );
 
 
-        // QUESTION 3 - LEAP YEAR (IMP QUESTION)
-        System.out.print(" Enter year :");
-        int year = in.nextInt();
-        boolean leap = ( year %4 == 0 && year %100!=0) || (year % 400 ==0); // year %400 checks for century years 
-        System.out.println("leap year ? : " + leap);
+        // // QUESTION 3 - LEAP YEAR (IMP QUESTION)
+        // System.out.print(" Enter year :");
+        // int year = in.nextInt();
+        // boolean leap = ( year %4 == 0 && year %100!=0) || (year % 400 ==0); // year %400 checks for century years 
+        // System.out.println("leap year ? : " + leap);
+
+
+        // // QUESTION 4 - MINUTES IN HOURS 
+        // int minutes = 150 ;
+        // int hours = minutes / 60 ;
+        // float min = (minutes % 60);
+        // System.out.println( "hours "+ hours + " minutes :" + min);
+
+        // QUESTION 5 - PERCENTAGE OF THREE SUBJECTS MARKS 
+        System.out.print(" Enter makers of sub A , B , C : ");
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        float per = (( a+ b+ c )/3.0f );
+        System.out.println(per);
+
 
        
 
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,6 @@
+
+        // // QUESTION 4 - MINUTES IN HOURS 
+        // int minutes = 150 ;
+        // int hours = minutes / 60 ;
+        // float min = (minutes % 60);
+        // System.out.println( "hours "+ hours + " minutes :" + min);
