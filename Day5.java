b0b044e19526e47424ad8ed72334ba8322c60821
jaanvigangwal22 <jaanvi.gangwal04@gmail.com>
2026-07-18 23:55:03 +0530

    update

--- Day5.java
+++ Day5.java
@@ -1,29 +1,69 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
 public class Day5{
     public static void main(String[] args) {
-        // Scanner in = new Scanner(System.in);
-        int x = 7;
-        int y = -5;
+        Scanner in = new Scanner(System.in);
+        // int x = 7;
+        // int y = -5;
 
-        if(x>0 && y>0){
-            System.out.println("QUADRANT 1");
+        // if(x>0 && y>0){
+        //     System.out.println("QUADRANT 1");
+        // }
+        //  if(x<0 && y>0){
+        //     System.out.println("QUADRANT 2");
+        // }
+        //  if(x<0 && y<0){
+        //     System.out.println("QUADRANT 3");
+        // }
+        //  if(x>0 && y<0){
+        //     System.out.println("QUADRANT 3");
+        // }
+        // else if( x==0 && y==0){
+        //     System.out.println("ORIGIN");
+        // }
+        // else{
+        //     System.out.println("ERROR");
+        // }
+        System.out.print("enter day:");
+        int day = in.nextInt();
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                
+                break;
+                case 2:
+                System.out.println("Tuesday");
+                
+                break;
+                 case 3:
+                System.out.println("wednesday");
+                
+                break;
+                 case 4:
+                System.out.println("Thursday");
+                
+                break;
+                 case 5:
+                System.out.println("Friday");
+                
+                break;
+                 case 6:
+                System.out.println("Saturday");
+                
+                break;
+                 case 7:
+                System.out.println("Sunday");
+                
+                break;
+                
+        
+            default:
+                System.out.println("Enter day .");
+            
+                break;
         }
-         if(x<0 && y>0){
-            System.out.println("QUADRANT 2");
-        }
-         if(x<0 && y<0){
-            System.out.println("QUADRANT 3");
-        }
-         if(x>0 && y<0){
-            System.out.println("QUADRANT 3");
-        }
-        else if( x==0 && y==0){
-            System.out.println("ORIGIN");
-        }
-        else{
-            System.out.println("ERROR");
-        }
+
+        
         
 
         
