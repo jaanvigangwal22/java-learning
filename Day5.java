e8b7f249b46ff38e41df870ee71bb2d04520c88d
jaanvigangwal22 <jaanvi.gangwal04@gmail.com>
2026-07-18 23:47:31 +0530

    Quadrant check

--- Day5.java
+++ Day5.java
@@ -1,48 +1,32 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class Day5 {
+public class Day5{
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        
-        //QUESTION 1 - CALCULATOR 
-        while(true){
-            int answer = 0;
-            System.out.print("Enter operator :");
-            char op = in.next().trim().charAt(0);
+        // Scanner in = new Scanner(System.in);
+        int x = 7;
+        int y = -5;
 
-            if(op == '+'|| op=='-' || op=='*'|| op=='/'){
-                System.out.print("Enter a and b :");
-                int a = in.nextInt();
-                int b = in.nextInt();
-                
-            
-                if(op == '+'){
-                    answer = a+b;
-                }
-                if(op == '-'){
-                    answer= a-b;
-                }
-                if (op == '*'){
-                    answer = a*b;
-                }
-                if( op =='/'){
-                    if(b!=0){
-                        answer= a/b;
-                    }
-                    else{
-                        System.out.println("Enter valid number ");
-                    }
-                }
-            
-            }
-            else if( op =='x'|| op =='X'){
-                break;
-            }
-            else{
-                System.out.println("Enter Valid operator");
-            }
+        if(x>0 && y>0){
+            System.out.println("QUADRANT 1");
         }
-        System.out.println(answer);
+         if(x<0 && y>0){
+            System.out.println("QUADRANT 2");
+        }
+         if(x<0 && y<0){
+            System.out.println("QUADRANT 3");
+        }
+         if(x>0 && y<0){
+            System.out.println("QUADRANT 3");
+        }
+        else if( x==0 && y==0){
+            System.out.println("ORIGIN");
+        }
+        else{
+            System.out.println("ERROR");
+        }
+        
+
+        
 
     }
     
